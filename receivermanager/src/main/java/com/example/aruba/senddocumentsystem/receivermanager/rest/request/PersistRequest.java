ee2e7def6b5be79b539cfff228246be7b31d6e1f
AlessioDoti <alessio.doti@gmail.com>
2025-06-29 23:02:12 +0200

    added forgotten tests

--- receivermanager/src/main/java/com/example/aruba/senddocumentsystem/receivermanager/rest/request/PersistRequest.java
+++ receivermanager/src/main/java/com/example/aruba/senddocumentsystem/receivermanager/rest/request/PersistRequest.java
@@ -1,8 +1,10 @@
 package com.example.aruba.senddocumentsystem.receivermanager.rest.request;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public abstract class PersistRequest extends BaseRequest{
 
     protected String receiverKey;
